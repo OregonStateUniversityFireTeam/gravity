1db24579b68e4528977ac50db01480403ea040fc
Sean McGregor <smcgregor08@cmc.edu>
2015-04-14 09:36:22 -0700

    added simplistic test for errors

--- specs/tc_optimizer.py
+++ specs/tc_optimizer.py
@@ -1,10 +1,19 @@
-from FireGirl_Optimizer import *
 import unittest
 
 class TestOptimization(unittest.TestCase):
 
-  def test_hello_world(self):
-    self.assertIsNotNone("output")
+    def test_does_not_error(self):
+
+        from FireGirlOptimizer import FireGirlPolicyOptimizer
+        from FireGirlPolicy import FireGirlPolicy
+
+        FGPO = FireGirlPolicyOptimizer()
+        FGPO.createFireGirlPathways(5,5)
+        b = [0,0,0,0,0,0,0,0,0,0,0]
+        policy = FireGirlPolicy(b)
+        FGPO.setPolicy(policy)
+
+        self.assertIsNotNone("Hello World")
 
 if __name__ == '__main__':
     unittest.main()
