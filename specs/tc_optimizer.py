2e04490cd7a87804a9625c1674439d99e0a1a333
Sean McGregor <smcgregor08@cmc.edu>
2015-04-13 21:58:52 -0700

    added hello world test case

---
+++ specs/tc_optimizer.py
@@ -0,0 +1,10 @@
+from FireGirl_Optimizer import *
+import unittest
+
+class TestOptimization(unittest.TestCase):
+
+  def test_hello_world(self):
+    self.assertIsNotNone("output")
+
+if __name__ == '__main__':
+    unittest.main()
